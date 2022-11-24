a9c8d38801cdcc9fbb2b51d7336b4bf7ac1259f8
Any97Cris <criscianysilva1997@gmail.com>
2022-11-24 15:09:05 -0400

    criei o controller para cadastrar

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -8,8 +8,21 @@ use App\Models\Contacts;
 class ContactsController extends Controller
 {
     public function index(){
-        $contatos = Contacts::all();
+        $contatos = Contacts::all();       
 
         return view('index', ['contatos' => $contatos]);
+    }
+
+    public function create(Request $request){
+        $contatos = new Contacts;
+
+        $contatos->name = $request->name;
+        $contatos->numberContact = $request->numberContact;
+        $contatos->email = $request->email;
+        $contatos->status = $request->status;
+
+        $contatos->save();
+
+        return redirect('/');
     }
 }
